4c1185da14eb0acc25c69c34cb242fe6ffd26c5a
Shikai Jin <79543449+shikaijin@users.noreply.github.com>
2021-07-31 22:48:14 -0400

    Add files via upload

---
+++ Motorcycle.py
@@ -0,0 +1,19 @@
+# Motorcycle Class
+# Zoe Jin
+
+from Vehicle import Vehicle
+
+class Motorcycle(Vehicle):
+    def _init_(self):
+        self.no_of_trims = no_of_trims
+        super(Motorcycle,self)._init_()
+
+# Unique Method
+    def WheelNumber(self):
+        wn = self.no_of_trims
+        return wn
+
+# Overriden method
+    def Detail(self):
+        detail = "make:"+ self.make +",year:"+ str(self.year) + ",price:"+ str(self.price) + ",trims:"+ str(self.no_of_trims)
+        return detail
