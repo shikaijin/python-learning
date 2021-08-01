d9a3347262b915b14f7611eb7f30c64e1665c319
Shikai Jin <79543449+shikaijin@users.noreply.github.com>
2021-07-31 22:51:20 -0400

    Update TestV.py

--- TestV.py
+++ TestV.py
@@ -27,22 +27,22 @@ m.price = 10000
 m.no_of_trims = 2
 
 # accessing inherited variables
-print("Car status:",c.status())
-print("Truck status:",t.status())
-print("Motocycle status:",m.status())
+print("Car status:", c.status())
+print("Truck status:", t.status())
+print("Motocycle status:", m.status())
 
 # accessing inherited methods
-print("The car is sold in",c.SoldYear())
-print("The truck is sold in",t.SoldYear())
-print("The motocycle is sold in",m.SoldYear())
+print("The car is sold in", c.SoldYear())
+print("The truck is sold in", t.SoldYear())
+print("The motocycle is sold in", m.SoldYear())
 
 # accessing overridden methods
-print("Car details:",c.Detail())
-print("Truck details:",t.Detail())
-print("Motorcycle details:",m.Detail())
+print("Car details:", c.Detail())
+print("Truck details:", t.Detail())
+print("Motorcycle details:", m.Detail())
 
 
 # accessing unique methods
-print("PassengerCapacity:",c.PassengerCapacity())
-print("ContainerCapacity:",t.ContainerCapacity())
-print("WheelNumber:",m.WheelNumber())
+print("PassengerCapacity:", c.PassengerCapacity())
+print("ContainerCapacity:", t.ContainerCapacity())
+print("WheelNumber:", m.WheelNumber())
